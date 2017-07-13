551dfb17f94a69b69c0f495f748a9abd5679bce1
“Rena” <renaadler1@gmail.com>
2017-07-12 20:15:26 -0500

    changed many things

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def full_name
+    first_name + " " + last_name
+  end
+
+  def racist
+    " +81 " + phone_number.to_s
+  end
 end
