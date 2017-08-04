2d1dc98b6b817e1fd030bf7ba7a8fbdc023ffeb7
“Rena” <renaadler1@gmail.com>
2017-08-04 13:43:33 -0500

    added validations for contact, happy/sad paths for create action and new.html.erb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -27,7 +27,7 @@ class ContactsController < ApplicationController
   end
 
   def create
-    contact = Contact.new(
+    @contact = Contact.new(
       first_name: params[:first_name],
       middle_name: params[:middle_name],
       last_name: params[:last_name],
@@ -36,8 +36,12 @@ class ContactsController < ApplicationController
       bio: params[:bio],
       user_id: current_user.id
       )
-    contact.save
-    redirect_to "/contacts/#{contact.id}"
+    if contact.save
+      flash[:success] = "Contact created."
+      redirect_to "/contacts/#{contact.id}"
+    else
+      render "new.html.erb"
+    end
   end
 
   def show
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
   belongs_to :user
   has_many :contact_groups
   has_many :groups, through: :contact_groups
